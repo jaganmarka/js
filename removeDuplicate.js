9fc43e48f52743529953afd87d7edee3e7412738
jaganmarka <30289505+jaganmarka@users.noreply.github.com>
2020-09-11 00:19:33 +0530

    Create removeDuplicate.js

---
+++ removeDuplicate.js
@@ -0,0 +1,410 @@
+const shares = [];
+let obj = [
+    {
+      "id": 92353,
+      "name": "Screen Shot 2020-09-07 at 10.22.59 PM.png",
+      "parent_id": null,
+      "bucket_name": "flujo-user-ALPHA-2-5",
+      "location": "",
+      "provider": "Wasabi",
+      "preview": null,
+      "type": "File",
+      "is_trash": false,
+      "real_path": "flujo-ws-2/1599542729574#FLUJO#Screen Shot 2020-09-07 at 10.22.59 PM.png",
+      "createdAt": "2020-09-08T05:25:34.344Z",
+      "updatedAt": "2020-09-08T05:25:34.344Z",
+      "obj_priveleges": [
+        {
+          "id": 32430,
+          "user_id": 5,
+          "privelege_id": 7,
+          "object_id": 92353,
+          "createdAt": "2020-09-08T05:25:34.363Z",
+          "updatedAt": "2020-09-08T05:25:34.363Z"
+        }
+      ],
+      "obj_attributes": [
+        {
+          "id": 40238,
+          "user_id": 5,
+          "object_id": 92353,
+          "attribute_id": 1,
+          "value": "png",
+          "createdAt": "2020-09-08T05:25:34.363Z",
+          "updatedAt": "2020-09-08T05:25:34.363Z"
+        },
+        {
+          "id": 40239,
+          "user_id": 5,
+          "object_id": 92353,
+          "attribute_id": 2,
+          "value": "78717",
+          "createdAt": "2020-09-08T05:25:34.363Z",
+          "updatedAt": "2020-09-08T05:25:34.363Z"
+        }
+      ],
+      "team_obj_attributes": [],
+      "task_obj_attributes": [],
+      "obj_shares": [
+        {
+          "id": 1347,
+          "from_user_id": 5,
+          "to_user_id": 15,
+          "object_id": 92353,
+          "shared_location": "Screen Shot 2020-09-07 at 10.22.59 PM.png",
+          "status": 1,
+          "createdAt": "2020-09-08T12:26:01.527Z",
+          "updatedAt": "2020-09-08T12:26:01.527Z"
+        },
+        {
+          "id": 1350,
+          "from_user_id": 5,
+          "to_user_id": 11,
+          "object_id": 92353,
+          "shared_location": "Screen Shot 2020-09-07 at 10.22.59 PM.png",
+          "status": 1,
+          "createdAt": "2020-09-08T12:26:01.532Z",
+          "updatedAt": "2020-09-08T12:26:01.532Z"
+        }
+      ],
+      "obj_trashes": [],
+      "obj_stars": []
+    },
+    {
+      "id": 92361,
+      "name": "flujo-user-ALPHA-2-8-flujo-team-2-1355/Screen Shot 2020-09-07 at 10.22.59 PM.png",
+      "parent_id": null,
+      "bucket_name": "flujo-user-ALPHA-2-5",
+      "location": "flujo-user-ALPHA-2-8-flujo-team-2-1355/",
+      "provider": "Wasabi",
+      "preview": null,
+      "type": "File",
+      "is_trash": false,
+      "real_path": "flujo-ws-2/1599542729574#FLUJO#Screen Shot 2020-09-07 at 10.22.59 PM.png",
+      "createdAt": "2020-09-08T06:43:23.818Z",
+      "updatedAt": "2020-09-08T06:43:23.818Z",
+      "obj_priveleges": [
+        {
+          "id": 32433,
+          "user_id": 5,
+          "privelege_id": 7,
+          "object_id": 92361,
+          "createdAt": "2020-09-08T06:43:23.855Z",
+          "updatedAt": "2020-09-08T06:43:23.855Z"
+        }
+      ],
+      "obj_attributes": [
+        {
+          "id": 40242,
+          "user_id": 5,
+          "object_id": 92361,
+          "attribute_id": 1,
+          "value": "png",
+          "createdAt": "2020-09-08T06:43:23.855Z",
+          "updatedAt": "2020-09-08T06:43:23.855Z"
+        },
+        {
+          "id": 40243,
+          "user_id": 5,
+          "object_id": 92361,
+          "attribute_id": 2,
+          "value": "78717",
+          "createdAt": "2020-09-08T06:43:23.855Z",
+          "updatedAt": "2020-09-08T06:43:23.855Z"
+        }
+      ],
+      "team_obj_attributes": [],
+      "task_obj_attributes": [],
+      "obj_shares": [],
+      "obj_trashes": [],
+      "obj_stars": []
+    },
+    {
+      "id": 92362,
+      "name": "(copy)-Screen Shot 2020-09-07 at 10.22.59 PM.png",
+      "parent_id": null,
+      "bucket_name": "flujo-user-ALPHA-2-5",
+      "location": "",
+      "provider": "Wasabi",
+      "preview": null,
+      "type": "File",
+      "is_trash": false,
+      "real_path": "flujo-ws-2/1599542729574#FLUJO#Screen Shot 2020-09-07 at 10.22.59 PM.png",
+      "createdAt": "2020-09-08T06:43:23.829Z",
+      "updatedAt": "2020-09-08T06:43:23.829Z",
+      "obj_priveleges": [
+        {
+          "id": 32434,
+          "user_id": 5,
+          "privelege_id": 7,
+          "object_id": 92362,
+          "createdAt": "2020-09-08T06:43:23.861Z",
+          "updatedAt": "2020-09-08T06:43:23.861Z"
+        }
+      ],
+      "obj_attributes": [
+        {
+          "id": 40244,
+          "user_id": 5,
+          "object_id": 92362,
+          "attribute_id": 1,
+          "value": "png",
+          "createdAt": "2020-09-08T06:43:23.861Z",
+          "updatedAt": "2020-09-08T06:43:23.861Z"
+        },
+        {
+          "id": 40245,
+          "user_id": 5,
+          "object_id": 92362,
+          "attribute_id": 2,
+          "value": "78717",
+          "createdAt": "2020-09-08T06:43:23.861Z",
+          "updatedAt": "2020-09-08T06:43:23.861Z"
+        }
+      ],
+      "team_obj_attributes": [],
+      "task_obj_attributes": [],
+      "obj_shares": [
+        {
+          "id": 1345,
+          "from_user_id": 5,
+          "to_user_id": 15,
+          "object_id": 92362,
+          "shared_location": "(copy)-Screen Shot 2020-09-07 at 10.22.59 PM.png",
+          "status": 1,
+          "createdAt": "2020-09-08T12:26:01.516Z",
+          "updatedAt": "2020-09-08T12:26:01.516Z"
+        },
+        {
+          "id": 1346,
+          "from_user_id": 5,
+          "to_user_id": 11,
+          "object_id": 92362,
+          "shared_location": "(copy)-Screen Shot 2020-09-07 at 10.22.59 PM.png",
+          "status": 1,
+          "createdAt": "2020-09-08T12:26:01.516Z",
+          "updatedAt": "2020-09-08T12:26:01.516Z"
+        },
+        {
+          "id": 1368,
+          "from_user_id": 5,
+          "to_user_id": 14,
+          "object_id": 92362,
+          "shared_location": "(copy)-Screen Shot 2020-09-07 at 10.22.59 PM.png",
+          "status": 1,
+          "createdAt": "2020-09-10T05:51:27.084Z",
+          "updatedAt": "2020-09-10T05:51:27.084Z"
+        },
+        {
+          "id": 1369,
+          "from_user_id": 5,
+          "to_user_id": 10,
+          "object_id": 92362,
+          "shared_location": "(copy)-Screen Shot 2020-09-07 at 10.22.59 PM.png",
+          "status": 1,
+          "createdAt": "2020-09-10T05:56:12.048Z",
+          "updatedAt": "2020-09-10T05:56:12.048Z"
+        }
+      ],
+      "obj_trashes": [],
+      "obj_stars": []
+    },
+    {
+      "id": 92377,
+      "name": "MESSENGER/5-11/(copy)-Screen Shot 2020-09-07 at 10.22.59 PM.png",
+      "parent_id": null,
+      "bucket_name": "flujo-user-ALPHA-2-5",
+      "location": "MESSENGER/5-11/",
+      "provider": "Wasabi",
+      "preview": null,
+      "type": "File",
+      "is_trash": false,
+      "real_path": "flujo-ws-2/1599542729574#FLUJO#Screen Shot 2020-09-07 at 10.22.59 PM.png",
+      "createdAt": "2020-09-08T12:26:01.429Z",
+      "updatedAt": "2020-09-08T12:26:01.429Z",
+      "obj_priveleges": [
+        {
+          "id": 32444,
+          "user_id": 5,
+          "privelege_id": 7,
+          "object_id": 92377,
+          "createdAt": "2020-09-08T12:26:01.447Z",
+          "updatedAt": "2020-09-08T12:26:01.447Z"
+        }
+      ],
+      "obj_attributes": [
+        {
+          "id": 40252,
+          "user_id": 5,
+          "object_id": 92377,
+          "attribute_id": 1,
+          "value": "png",
+          "createdAt": "2020-09-08T12:26:01.448Z",
+          "updatedAt": "2020-09-08T12:26:01.448Z"
+        },
+        {
+          "id": 40253,
+          "user_id": 5,
+          "object_id": 92377,
+          "attribute_id": 2,
+          "value": "78717",
+          "createdAt": "2020-09-08T12:26:01.448Z",
+          "updatedAt": "2020-09-08T12:26:01.448Z"
+        }
+      ],
+      "team_obj_attributes": [],
+      "task_obj_attributes": [],
+      "obj_shares": [],
+      "obj_trashes": [],
+      "obj_stars": []
+    },
+    {
+      "id": 92378,
+      "name": "MESSENGER/5-15/(copy)-Screen Shot 2020-09-07 at 10.22.59 PM.png",
+      "parent_id": null,
+      "bucket_name": "flujo-user-ALPHA-2-5",
+      "location": "MESSENGER/5-15/",
+      "provider": "Wasabi",
+      "preview": null,
+      "type": "File",
+      "is_trash": false,
+      "real_path": "flujo-ws-2/1599542729574#FLUJO#Screen Shot 2020-09-07 at 10.22.59 PM.png",
+      "createdAt": "2020-09-08T12:26:01.463Z",
+      "updatedAt": "2020-09-08T12:26:01.463Z",
+      "obj_priveleges": [
+        {
+          "id": 32445,
+          "user_id": 5,
+          "privelege_id": 7,
+          "object_id": 92378,
+          "createdAt": "2020-09-08T12:26:01.483Z",
+          "updatedAt": "2020-09-08T12:26:01.483Z"
+        }
+      ],
+      "obj_attributes": [
+        {
+          "id": 40254,
+          "user_id": 5,
+          "object_id": 92378,
+          "attribute_id": 1,
+          "value": "png",
+          "createdAt": "2020-09-08T12:26:01.484Z",
+          "updatedAt": "2020-09-08T12:26:01.484Z"
+        },
+        {
+          "id": 40255,
+          "user_id": 5,
+          "object_id": 92378,
+          "attribute_id": 2,
+          "value": "78717",
+          "createdAt": "2020-09-08T12:26:01.484Z",
+          "updatedAt": "2020-09-08T12:26:01.484Z"
+        }
+      ],
+      "team_obj_attributes": [],
+      "task_obj_attributes": [],
+      "obj_shares": [],
+      "obj_trashes": [],
+      "obj_stars": []
+    },
+    {
+      "id": 92381,
+      "name": "MESSENGER/5-15/Screen Shot 2020-09-07 at 10.22.59 PM.png",
+      "parent_id": null,
+      "bucket_name": "flujo-user-ALPHA-2-5",
+      "location": "MESSENGER/5-15/",
+      "provider": "Wasabi",
+      "preview": null,
+      "type": "File",
+      "is_trash": false,
+      "real_path": "flujo-ws-2/1599542729574#FLUJO#Screen Shot 2020-09-07 at 10.22.59 PM.png",
+      "createdAt": "2020-09-08T12:26:01.515Z",
+      "updatedAt": "2020-09-08T12:26:01.515Z",
+      "obj_priveleges": [
+        {
+          "id": 32448,
+          "user_id": 5,
+          "privelege_id": 7,
+          "object_id": 92381,
+          "createdAt": "2020-09-08T12:26:01.563Z",
+          "updatedAt": "2020-09-08T12:26:01.563Z"
+        }
+      ],
+      "obj_attributes": [
+        {
+          "id": 40258,
+          "user_id": 5,
+          "object_id": 92381,
+          "attribute_id": 1,
+          "value": "png",
+          "createdAt": "2020-09-08T12:26:01.563Z",
+          "updatedAt": "2020-09-08T12:26:01.563Z"
+        },
+        {
+          "id": 40259,
+          "user_id": 5,
+          "object_id": 92381,
+          "attribute_id": 2,
+          "value": "78717",
+          "createdAt": "2020-09-08T12:26:01.563Z",
+          "updatedAt": "2020-09-08T12:26:01.563Z"
+        }
+      ],
+      "team_obj_attributes": [],
+      "task_obj_attributes": [],
+      "obj_shares": [],
+      "obj_trashes": [],
+      "obj_stars": []
+    },
+    {
+      "id": 92380,
+      "name": "MESSENGER/5-11/Screen Shot 2020-09-07 at 10.22.59 PM.png",
+      "parent_id": null,
+      "bucket_name": "flujo-user-ALPHA-2-5",
+      "location": "MESSENGER/5-11/",
+      "provider": "Wasabi",
+      "preview": null,
+      "type": "File",
+      "is_trash": false,
+      "real_path": "flujo-ws-2/1599542729574#FLUJO#Screen Shot 2020-09-07 at 10.22.59 PM.png",
+      "createdAt": "2020-09-08T12:26:01.504Z",
+      "updatedAt": "2020-09-08T12:26:01.504Z",
+      "obj_priveleges": [
+        {
+          "id": 32450,
+          "user_id": 5,
+          "privelege_id": 7,
+          "object_id": 92380,
+          "createdAt": "2020-09-08T12:26:01.563Z",
+          "updatedAt": "2020-09-08T12:26:01.563Z"
+        }
+      ],
+      "obj_attributes": [
+        {
+          "id": 40260,
+          "user_id": 5,
+          "object_id": 92380,
+          "attribute_id": 1,
+          "value": "png",
+          "createdAt": "2020-09-08T12:26:01.563Z",
+          "updatedAt": "2020-09-08T12:26:01.563Z"
+        },
+        {
+          "id": 40261,
+          "user_id": 5,
+          "object_id": 92380,
+          "attribute_id": 2,
+          "value": "78717",
+          "createdAt": "2020-09-08T12:26:01.563Z",
+          "updatedAt": "2020-09-08T12:26:01.563Z"
+        }
+      ],
+      "team_obj_attributes": [],
+      "task_obj_attributes": [],
+      "obj_shares": [],
+      "obj_trashes": [],
+      "obj_stars": []
+    }
+  ]
+obj.map(o => o.obj_shares.filter(f => shares.some(s => s.to_user_id === f.to_user_id) ? null : shares.push(f)));
+console.log(shares)
